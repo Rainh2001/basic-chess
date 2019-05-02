1efabdeac57c07cf80a9537787ae139c9042746b
Rain Holloway <rainh2001@gmail.com>
2019-05-02 10:26:54 +1000

    Added Board class with constructor that prints chess board to canvas and has a 2d array of possible positions

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 var canvas;
 var ctx;
+var board;
 
 const row = 8;
 const col = 8;
 
 var width;
 var height;
+
+class Board {
+    constructor(){
+        this.pos = [];
+        let isBlack = false;
+        for(let i = 0; i < col; i++){
+            this.pos.push([]);
+            isBlack = !isBlack;
+            for(let j = 0; j < row; j++){
+                this.pos[i].push({x: j*width, y: i*height});
+                ctx.beginPath(); 
+                ctx.fillStyle = isBlack ? "black" : "white";
+                ctx.fillRect(j*width, i*height, width, height);
+                ctx.closePath();
+                isBlack = !isBlack;
+            }
+        }
+    }
+}
 
 window.onload = function(){
     canvas = document.getElementById("chess");
@@ -18,15 +38,5 @@ window.onload = function(){
 }
 
 function setup(){
-    let isBlack = false;
-    for(let i = 0; i < row; i++){
-        isBlack = !isBlack;
-        for(let j = 0; j < col; j++){
-            ctx.beginPath(); 
-            ctx.fillStyle = isBlack ? "black" : "white";
-            ctx.fillRect(j*width, i*height, width, height);
-            ctx.closePath();
-            isBlack = !isBlack;
-        }
-    }
+    board = new Board();
 }
