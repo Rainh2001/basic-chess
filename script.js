61d396ee1e2a39abac210150dc2af2fcdac5529e
Rainh2001 <rainh2001@gmail.com>
2019-05-06 09:56:26 +1000

    Implemented cursor position functions: determines the chess piece the user clicks on

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ class Board {
             this.pos.push([]);
             isBlack = !isBlack;
             for(let j = 0; j < row; j++){
-                this.pos[i].push({x: j*width, y: i*height});
+                this.pos[i].push(new Position(j*width, i*height));
                 this.pos[i][j].color = isBlack ? "black" : "white";
                 ctx.beginPath(); 
                 ctx.fillStyle = this.pos[i][j].color;
@@ -28,6 +28,15 @@ class Board {
                 isBlack = !isBlack;
             }
         }
+    }
+}
+
+class Position {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.state = false;
+        this.currentPiece;
     }
 }
 
@@ -109,7 +118,9 @@ window.onload = function(){
 
     width = canvas.width / col;
     height = canvas.height / row;
-    
+
+    canvas.addEventListener("click", getMousePos);
+
     setup();
 }
 
@@ -128,6 +139,8 @@ function setup(){
         for(let j = 0; j < col; j++){
             let pawnLine = i === 0 ? 1 : 6;
             pawn.push(new Pawn(isBlack, board.pos[pawnLine][j]));
+            board.pos[pawnLine][j].state = true;
+            board.pos[pawnLine][j].currentPiece = pawn[j];
             pawn[j].img.onload = function(){
                 pawn[j].draw();
             }
@@ -139,14 +152,20 @@ function setup(){
         let knight = [];
         for(let j = 0; j < 2; j++){
             rook.push(new Rook(isBlack, board.pos[line][j*7]));
+            board.pos[line][j*7].state = true;
+            board.pos[line][j*7].currentPiece = rook[j];
             rook[j].img.onload = function(){
                 rook[j].draw();
             }
             bishop.push(new Bishop(isBlack, board.pos[line][j*3+2]));
+            board.pos[line][j*3+2].state = true;
+            board.pos[line][j*3+2].currentPiece = bishop[j];
             bishop[j].img.onload = function(){
                 bishop[j].draw();
             }
             knight.push(new Knight(isBlack, board.pos[line][j*5+1]));
+            board.pos[line][j*5+1].state = true;
+            board.pos[line][j*5+1].currentPiece = knight[j];
             knight[j].img.onload = function(){
                 knight[j].draw();
             }
@@ -154,12 +173,16 @@ function setup(){
 
         // Queen creation
         let queen = new Queen(isBlack, board.pos[line][4]);
+        board.pos[line][4].state = true;
+        board.pos[line][4].currentPiece = queen;
         queen.img.onload = function(){
             queen.draw();
         }
 
         // King creation
         let king = new King(isBlack, board.pos[line][3]);
+        board.pos[line][3].state = true;
+        board.pos[line][3].currentPiece = king;
         king.img.onload = function(){
             king.draw();
         }
@@ -180,5 +203,30 @@ function setup(){
             white.king = king;
         }
     }
-    console.log({White: white, Black: black});
+}
+
+function getMousePos(event){
+    let pos = {
+        x: event.pageX - this.offsetLeft,
+        y: event.pageY - this.offsetTop
+    }
+    choosePiece(pos);
+}
+
+function choosePiece(pos){
+    for(let i = 0; i < board.pos.length; i++){
+        for(let j = 0; j < board.pos.length; j++){
+            if(pos.x >= board.pos[i][j].x && pos.x <= board.pos[i][j].x + width){
+                if(pos.y >= board.pos[i][j].y && pos.y <= board.pos[i][j].y + height){
+                    if(board.pos[i][j].state){
+                        startMove(board.pos[i][j].currentPiece);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function startMove(piece){
+    console.log(piece);
 }
